a9ae91239dd9fe14873ddeeeda142a0cb705a1c2
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-28 14:56:54 -0300

    Update createmyvmv2.0.sh

--- createmyvmv2.0.sh
+++ createmyvmv2.0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# mycreatevm script - create virtual machine using virtinstall
+# mycreatevm script - create virtual machine using virtinstall (goto menu style)
 # version 2.0 - build 28/10/2024
 # by dejavudf
 
