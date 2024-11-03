970f8a5192c6b6a969bb7298fbca830e260ff4ce
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-11-03 09:43:50 -0300

    Add files via upload

--- backup_gitbhub_repositoryv1.0.sh
+++ backup_gitbhub_repositoryv1.0.sh
@@ -6,25 +6,25 @@
 
 rm -Rf /data/temp/backup_config/*
 find ./ -type f | grep -i ".unl" > ./lista.txt
-cat ./lista.txt | while read VAR_FILE
+cat < /lista.txt | while read -r VAR_FILE
 do
-	VAR_LAB_NAME=$(cat "$VAR_FILE" | grep -i "<lab name=" | awk -F"=\"" '{print $2}' | sed 's\.unl:<lab name\\g' | sed 's\" id\\g')
-	VAR_LAB_ID=$(cat "$VAR_FILE" | grep -i "<lab name=" | awk -F"=\"" '{print $3}' | sed 's\" version\\g')
-	VAR_NODE=$(cat "$VAR_FILE" | grep -i "<node id=" | awk -F"=\"" '{print $3,$2}' | sed 's\" type \#\g' | sed 's\" name\\g')
-	echo "$VAR_NODE" > /tmp/$VAR_LAB_ID.lab
-	cat /tmp/$VAR_LAB_ID.lab | while read VAR_LAB_FILE
+	VAR_LAB_NAME=$(cat < "$VAR_FILE" | grep -i "<lab name=" | awk -F"=\"" '{print $2}' | sed 's\.unl:<lab name\\g' | sed 's\" id\\g')
+	VAR_LAB_ID=$(cat < "$VAR_FILE" | grep -i "<lab name=" | awk -F"=\"" '{print $3}' | sed 's\" version\\g')
+	VAR_NODE=$(cat < "$VAR_FILE" | grep -i "<node id=" | awk -F"=\"" '{print $3,$2}' | sed 's\" type \#\g' | sed 's\" name\\g')
+	echo "$VAR_NODE" > /tmp/"$VAR_LAB_ID".lab
+	cat < /tmp/"$VAR_LAB_ID".lab | while read -r VAR_LAB_FILE
 	do
 	VAR_CONFIG_NAME=$(echo "$VAR_LAB_FILE" | awk -F"#" '{print $1}')
 	VAR_CONFIG_ID=$(echo "$VAR_LAB_FILE" | awk -F"#" '{print $2}')
-	if [ -d /opt/unetlab/tmp/0/$VAR_LAB_ID ]
+	if [ -d /opt/unetlab/tmp/0/"$VAR_LAB_ID" ]
 	then
-		if [ ! -d /data/temp/backup_config/$VAR_LAB_ID ]
+		if [ ! -d /data/temp/backup_config/"$VAR_LAB_ID" ]
 		then
-			mkdir /data/temp/backup_config/$VAR_LAB_ID
+			mkdir /data/temp/backup_config/"$VAR_LAB_ID"
 			echo "$VAR_LAB_ID" > "/data/temp/backup_config/$VAR_LAB_ID/$VAR_LAB_NAME.txt"
 			#echo "$VAR_LAB_NAME" > /data/temp/backup_config/$VAR_LAB_ID/$VAR_LAB_ID.txt
 		else
-			cp /opt/unetlab/tmp/0/$VAR_LAB_ID/$VAR_CONFIG_ID/startup-config /data/temp/backup_config/$VAR_LAB_ID/$VAR_CONFIG_NAME.cfg
+			cp /opt/unetlab/tmp/0/"$VAR_LAB_ID"/"$VAR_CONFIG_ID"/startup-config /data/temp/backup_config/"$VAR_LAB_ID"/"$VAR_CONFIG_NAME".cfg
 			cp "$VAR_FILE" "/data/temp/backup_config/$VAR_LAB_ID/"
 		fi
 	else
