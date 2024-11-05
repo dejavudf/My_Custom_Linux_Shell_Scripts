c0c9cee116dfd48ad8de9c4bca287c6601437fc7
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-11-05 18:52:35 -0300

    Update backup_gitbhub_repositoryv1.0.sh

--- backup_gitbhub_repositoryv1.0.sh
+++ backup_gitbhub_repositoryv1.0.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+# by dejavudf
+# Github Repository Download and Compact (zip)
+VAR_DT=$(date '+Date%d_%m_%Y_Hour%H_%M_%S')
+VAR_FILE=""
+
+clear
+if [ -f ./gitlist.txt ] || [ ! -r ./gitlist.txt ]
+then
+        for VAR_FILE in $(cat < ./gitlist.txt)
+        do
+                echo "Downloading repository. Wait..."
+                if git clone https://github.com/dejavudf/"$VAR_FILE"
+                then
+                        echo "Success: Repository download completed"
+                        mv ./"$VAR_FILE" ./github-dejavudf/"$VAR_FILE"
+                else
+                        echo "Error: Repository download not completed"
+                fi
+        done
+        echo "Compacting repositories. Wait..."
+        if tar -zcvf "$VAR_DT""bck_github.tgz" ./github-dejavudf
+        then
+                echo "Success: Repositories compact completed"
+                rm -Rf ./github-dejavudf/./*
+        else
+                echo "Error: Repositories compact not completed"
+        fi
+else
+        echo "Error: File gitlist.txt not found or not readable"
+        exit 1
+fi
+exit 0
+
 
