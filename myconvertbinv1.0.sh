82eb64d1775d212088a94d855dd32f2576e16670
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-11-21 10:02:38 -0300

    Update myconvertbinv1.0.sh

--- myconvertbinv1.0.sh
+++ myconvertbinv1.0.sh
@@ -24,34 +24,51 @@ FUNC_CONVERT() {
 	read VAR_VALUE
 	#DEC TO HEX
 	VAR_DEC_HEX=$(printf "%x\n" "$VAR_VALUE" 2> /dev/null)
-		echo "Decimal to Hexadecimal: $VAR_DEC_HEX"
 	#DEC TO OCT
 	VAR_DEC_OCT=$(printf "%o\n" "$VAR_VALUE" 2> /dev/null)
-		echo "Decimal to Octal: $VAR_DEC_OCT"
 	#DEC TO BIN
 	VAR_DEC_BIN=$(echo "obase=2;$VAR_VALUE" | bc 2> /dev/null)
-		echo "Decimal to Binary: $VAR_DEC_BIN"
 	#BIN TO HEX
 	VAR_BIN_HEX=$(printf "%x\n" $((2#$VAR_VALUE)) 2> /dev/null)
-		echo "Binary to Hexadecimal: $VAR_BIN_HEX"
 	#BIN TO DEC
 	VAR_BIN_DEC=$(printf "%d\n" $((2#$VAR_VALUE)) 2> /dev/null)
-		echo "Binary to Decimal: $VAR_BIN_DEC"
 	#BIN TO OCT
 	VAR_BIN_OCT=$(printf "%o\n" $((2#$VAR_VALUE)) 2> /dev/null)
-		echo "Binalry to Octal: $VAR_BIN_OCT"
 	#HEX TO DEC
 	VAR_HEX_DEC=$(printf "%d\n" 0x$VAR_VALUE 2> /dev/null)
-		echo "Hexadecimal to Decimal: $VAR_HEX_DEC"
 	#HEX TO OCT
 	VAR_HEX_OCT=$(printf "%o\n" 0x$VAR_VALUE 2> /dev/null)
+	#HEX TO BIN
+	#?
+	#OCT TO DEC
+	#OCT TO HEX
+	#OCT TO BIN
+ 	FUNC_SCREEN
+	FUNC_RESULT
+}
+
+FUNC_RESULT() {
+#DEC TO HEX
+		echo "Decimal to Hexadecimal: $VAR_DEC_HEX"
+	#DEC TO OCT
+		echo "Decimal to Octal: $VAR_DEC_OCT"
+	#DEC TO BIN
+		echo "Decimal to Binary: $VAR_DEC_BIN"
+	#BIN TO HEX
+		echo "Binary to Hexadecimal: $VAR_BIN_HEX"
+	#BIN TO DEC
+		echo "Binary to Decimal: $VAR_BIN_DEC"
+	#BIN TO OCT
+		echo "Binalry to Octal: $VAR_BIN_OCT"
+	#HEX TO DEC
+		echo "Hexadecimal to Decimal: $VAR_HEX_DEC"
+	#HEX TO OCT
 		echo "Hexadecimal to Octal: $VAR_HEX_OCT"
 	#HEX TO BIN
 	#?
 	#OCT TO DEC
 	#OCT TO HEX
 	#OCT TO BIN
-	sleep 10000
 }
 
 FUNC_CONVERT
