fbb8ff999f6fcc46853ec7568d9350ed3558cedd
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-05-20 17:34:35 -0300

    Update compare.sh

--- Curl_API/compare.sh
+++ Curl_API/compare.sh
@@ -2,28 +2,29 @@
 # by Alexsandro - github/dejavudf
 # v1.0 - 20/05/2025
 #variables
+#formato arquivo ipsopmon.txt: basta ter os ips dentros do arquiov (serviço e host)
+#formato arquivo ipsxmc: nome;localidade;IP
 
 rm ./listafinal.txt
 
 cat < ./ipsxmc.txt | while read -r VAR_XMC
 do
-	VAR_IP=$(echo "$VAR_XMC" | awk -v RS='\r\n' -F";" '{print $3}')
-	VAR_HOST=$(echo "$VAR_XMC" | awk -F";" '{print $1}')
-	VAR_LOCAL=$(echo "$VAR_XMC" | awk -F";" '{print $2}')
-	clear
-	echo 'Working: |'
-	clear
-	echo 'Working: /'
-	clear
-	echo 'Working: -'
-	clear
-	echo 'Working: \'
-	if ! cat ./ipsopmon.txt | grep -i "$VAR_IP"
-	then
-		echo "$VAR_IP"";""$VAR_HOST"";""$VAR_LOCAL" >> listafinal.txt
-	else
-		continue
-	fi
+        VAR_IP=$(echo "$VAR_XMC" | awk -v RS='\r\n' -F";" '{print $3}')
+        VAR_HOST=$(echo "$VAR_XMC" | awk -F";" '{print $1}')
+        VAR_LOCAL=$(echo "$VAR_XMC" | awk -F";" '{print $2}')
+        clear
+        echo 'Working: |'
+        clear
+        echo 'Working: /'
+        clear
+        echo 'Working: -'
+        clear
+        echo 'Working: \'
+        if grep -i "$VAR_IP" ./ipsopmon.txt
+        then
+                continue
+        else
+                echo "$VAR_IP"";""$VAR_HOST"";""$VAR_LOCAL" >> listafinal.txt
+        fi
 done
-}
 
