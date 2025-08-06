5254d910ad96f840ac3ba9d040749af8e7918554
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-08-06 12:59:04 -0300

    Update testeping2.sh

--- Curl_API/testeping2.sh
+++ Curl_API/testeping2.sh
@@ -3,8 +3,5 @@
 VAR_DATE=$(date '+%Y%m%d_%H:%M')
 VAR_TIME=1800
 
-nohup ping 177.23.109.90 -c $VAR_TIME > ./177.23.109.90_$VAR_DATE.txt &
-nohup ping 177.23.109.89 -c $VAR_TIME > ./177.23.109.89_$VAR_DATE.txt &
-nohup ping 200.169.70.129 -c $VAR_TIME > ./200.169.70.129_$VAR_DATE.txt &
-nohup ping 200.169.70.130 -c $VAR_TIME > ./200.169.70.130_$VAR_DATE.txt &
+nohup ping 1.1.1.1 -c $VAR_TIME > ./1.1.1.1.txt &
 
