e32d2a47659bb716642f6f178cb4220c1982cd50
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-08-08 09:51:33 -0300

    Update getdns.sh

--- Curl_API/getdns.sh
+++ Curl_API/getdns.sh
@@ -6,42 +6,44 @@ VAR_SLEEP=10
 VAR_TOTAL=360
 VAR_URL="cs-promptx-prd.brazil.communication.azure.com"
 
-rm ./logdns.txt
+if rm ./logdns.txt
+then
+	while [ "$VAR_CONTADOR" -le "$VAR_TOTAL" ]
+	do
+		clear
+		echo "Checking DNS: round "$VAR_CONTADOR" of "$VAR_TOTAL""". Please, wait..."
+		#google
+		dig @8.8.8.8 "$VAR_URL" >> ./logdns.txt
+		dig @8.8.4.4 "$VAR_URL" >> ./logdns.txt
+	
+		#cloudflare
+		dig @1.1.1.1 "$VAR_URL" >> ./logdns.txt
+		dig @1.0.0.1 "$VAR_URL" >> ./logdns.txt
 
-while [ "$VAR_CONTADOR" -le "30" ]
-do
-	clear
-	echo "Checking DNS: round "$VAR_CONTADOR" of "$VAR_TOTAL""". Please, wait..."
-	#google
-	dig @8.8.8.8 "$VAR_URL" >> ./logdns.txt
-	dig @8.8.4.4 "$VAR_URL" >> ./logdns.txt
+		#opendns
+		dig @208.67.222.222 "$VAR_URL" >> ./logdns.txt
+		dig @208.67.220.220 "$VAR_URL" >> ./logdns.txt
 
-	#cloudflare
-	dig @1.1.1.1 "$VAR_URL" >> ./logdns.txt
-	dig @1.0.0.1 "$VAR_URL" >> ./logdns.txt
+		#quad9
+		dig @9.9.9.9 "$VAR_URL" >> ./logdns.txt
+		dig @149.112.112.112 "$VAR_URL" >> ./logdns.txt
 
-	#opendns
-	dig @208.67.222.222 "$VAR_URL" >> ./logdns.txt
-	dig @208.67.220.220 "$VAR_URL" >> ./logdns.txt
+		#yandex
+		dig @77.88.8.8 "$VAR_URL" >> ./logdns.txt
+		dig @77.88.8.1 "$VAR_URL" >> ./logdns.txt
 
-	#quad9
-	dig @9.9.9.9 "$VAR_URL" >> ./logdns.txt
-	dig @149.112.112.112 "$VAR_URL" >> ./logdns.txt
+		#comodo
+		dig @8.26.56.26 "$VAR_URL" >> ./logdns.txt
+		dig @8.20.247.20 "$VAR_URL" >> ./logdns.txt
+		sleep 10
 
-	#yandex
-	dig @77.88.8.8 "$VAR_URL" >> ./logdns.txt
-	dig @77.88.8.1 "$VAR_URL" >> ./logdns.txt
-
-	#comodo
-	dig @8.26.56.26 "$VAR_URL" >> ./logdns.txt
-	dig @8.20.247.20 "$VAR_URL" >> ./logdns.txt
-	sleep 10
-
-	#count
-	VAR_CONTADOR=$(( "$VAR_CONTADOR" + 1 ))
-	sleep "$VAR_SLEEP"
-done
-
+		#count
+		VAR_CONTADOR=$(( "$VAR_CONTADOR" + 1 ))
+		sleep "$VAR_SLEEP"
+	done
+else
+	exit 1
+fi
 cat ./logdns.txt | grep -i "A" | grep IN | grep -iv CNAME | awk '{print $5}' | sort -du > lista_dns.txt
-
+exit 0
 
