6c0f94320b634e13d9af61b75ea6bc07d2b95544
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-08-08 08:34:02 -0300

    Update getdns.sh

--- Curl_API/getdns.sh
+++ Curl_API/getdns.sh
@@ -3,7 +3,8 @@
 #VAR
 VAR_CONTADOR=1
 VAR_SLEEP=10
-VAR_TOTAL=30
+VAR_TOTAL=360
+VAR_URL="cs-promptx-prd.brazil.communication.azure.com"
 
 rm ./logdns.txt
 
@@ -12,28 +13,28 @@ do
 	clear
 	echo "Checking DNS: round "$VAR_CONTADOR" of "$VAR_TOTAL""". Please, wait..."
 	#google
-	dig @8.8.8.8 login.microsoftonline.com > ./logdns.txt
-	dig @8.8.4.4 login.microsoftonline.com >> ./logdns.txt
+	dig @8.8.8.8 "$VAR_URL" > ./logdns.txt
+	dig @8.8.4.4 "$VAR_URL" >> ./logdns.txt
 
 	#cloudflare
-	dig @1.1.1.1 login.microsoftonline.com >> ./logdns.txt
-	dig @1.0.0.1 login.microsoftonline.com >> ./logdns.txt
+	dig @1.1.1.1 "$VAR_URL" >> ./logdns.txt
+	dig @1.0.0.1 "$VAR_URL" >> ./logdns.txt
 
 	#opendns
-	dig @208.67.222.222 login.microsoftonline.com >> ./logdns.txt
-	dig @208.67.220.220 login.microsoftonline.com >> ./logdns.txt
+	dig @208.67.222.222 "$VAR_URL" >> ./logdns.txt
+	dig @208.67.220.220 "$VAR_URL" >> ./logdns.txt
 
 	#quad9
-	dig @9.9.9.9 login.microsoftonline.com >> ./logdns.txt
-	dig @149.112.112.112 login.microsoftonline.com >> ./logdns.txt
+	dig @9.9.9.9 "$VAR_URL" >> ./logdns.txt
+	dig @149.112.112.112 "$VAR_URL" >> ./logdns.txt
 
 	#yandex
-	dig @77.88.8.8 login.microsoftonline.com >> ./logdns.txt
-	dig @77.88.8.1 login.microsoftonline.com >> ./logdns.txt
+	dig @77.88.8.8 "$VAR_URL" >> ./logdns.txt
+	dig @77.88.8.1 "$VAR_URL" >> ./logdns.txt
 
 	#comodo
-	dig @8.26.56.26 login.microsoftonline.com >> ./logdns.txt
-	dig @8.20.247.20  login.microsoftonline.com >> ./logdns.txt
+	dig @8.26.56.26 "$VAR_URL" >> ./logdns.txt
+	dig @8.20.247.20 "$VAR_URL" >> ./logdns.txt
 	sleep 10
 
 	#count
