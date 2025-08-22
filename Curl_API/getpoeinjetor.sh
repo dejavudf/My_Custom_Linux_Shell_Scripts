e22007eecbfca5e574e887f324b14c1f13006d72
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-08-22 10:11:34 -0300

    Update getpoeinjetor.sh

--- Curl_API/getpoeinjetor.sh
+++ Curl_API/getpoeinjetor.sh
@@ -3,21 +3,23 @@
 # version 1.0
 # debian/ubuntu/mint
 
+rm ./lista_extreme.txt
+
 echo "NOME_AP;MAC_AP;PORTA_AP;SWITCH_IP;SWITCH_NOME" > ./lista_extreme.txt
-find ./ -type f | grep -i ".txt" | while read -r VAR_FILE
+find ./ -type f | grep -i "10_" | while read -r VAR_FILE
 do
 	clear
 	echo "Lendo arquivo $VAR_FILE. Por favor, aguarde..."
 	VAR_SWITCH_IP=$(echo $VAR_FILE | awk -F'-' '{print $1}' | awk -F'_20250821' '{print $1}')
-	VAR_SWITCH_IP=$(echo $VAR_SWITCH_IP | sed 's/.\///g' | sed 's/_/./g' )
-	VAR_SWITCH_NOME=$(cat $VAR_FILE | grep -i '\.1 # ' | awk '{print $1}')
-	VAR_SWITCH_NOME=$(echo $VAR_SWITCH_NOME | sed 's/.1//g')
+	VAR_SWITCH_IP=$(echo $VAR_SWITCH_IP | sed 's/.\///g' | sed 's/_/\./g' )
+	VAR_SWITCH_NOME=$(cat $VAR_FILE | grep -i '\.1 # ' | sed 's/* //g' | awk '{print $1}')
+	VAR_SWITCH_NOME=$(echo $VAR_SWITCH_NOME | sed 's/\.1//g')
 	cat $VAR_FILE | grep -i -e "ge1" -e "eth0" | while read -r VAR_VIZINHO
 	do
 		VAR_AP_MAC=$(echo $VAR_VIZINHO | awk '{print $2}')
 		VAR_AP_NOME=$(echo $VAR_VIZINHO | awk '{print $6}')
 		VAR_AP_PORTA_UPLINK=$(echo $VAR_VIZINHO | awk '{print $1}')
-		if cat $VAR_FILE | grep -i "$VAR_AP_PORTA_UPLINK    searching"
+		if cat $VAR_FILE | grep -i "^$VAR_AP_PORTA_UPLINK  searching" || cat $VAR_FILE | grep -i "^$VAR_AP_PORTA_UPLINK   searching" || cat $VAR_FILE | grep -i "^$VAR_AP_PORTA_UPLINK    searching" || cat $VAR_FILE | grep -i "^$VAR_AP_PORTA_UPLINK     searching"
 		then
 			echo "$VAR_AP_NOME;$VAR_AP_MAC;$VAR_AP_PORTA_UPLINK;$VAR_SWITCH_IP;$VAR_SWITCH_NOME" >> ./lista_extreme.txt
 		else
@@ -25,4 +27,4 @@ do
 		fi
 	done
 done
-)
+exit 0
