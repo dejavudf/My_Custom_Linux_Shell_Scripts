a4026857beafb55c8700be9c35437288c2809706
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-11-13 14:42:57 -0300

    Update bck_treev1.0.sh

--- bck_treev1.0.sh
+++ bck_treev1.0.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# by alexsandro
+# by dejavudf - github.com/dejavudf
+# version 1.0 11/11/2025
+# debian/ubuntu/mint
 # compact and backup files and folders (zip)
 
 VAR_DT=$(date '+Date%d_%m_%Y_Hour%H_%M_%S')
 
 clear
-echo "Compacting repositories. Wait..."
+echo "Compacting files and folders. Wait..."
 tar -zcvf ./bck_yml_net/"$VAR_DT""bck_yml_net.tgz" ./*.txt ./scripts ./*.sh ./tests
 if [ $? == 0 ]
 then
