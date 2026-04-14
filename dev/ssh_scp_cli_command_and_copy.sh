0390f404658ba5fe1d98dc85f80642172219606a
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-04-14 12:39:23 -0300

    Create ssh_scp_cli_command_and_copy.sh

---
+++ dev/ssh_scp_cli_command_and_copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for VAR_IP in $(cat ip.txt)
+do
+        if sshpass -p password scp ./$1.xsf user@$VAR_IP:/usr/local/tmp/$1.xsf > "copy.log"
+        then
+                echo "$VAR_IP - Success" > success_copy.txt
+        else
+                echo "$VAR_IP - Failure" > failure_copy.txt
+        fi
+done
+
+
+#!/bin/bash
+
+for VAR_IP in $(cat ip.txt)
+do
+        if sshpass -p password ssh user@$VAR_IP -o RemoteCommand=$1 > "exec.log"
+        then
+                echo "$VAR_IP - Success" > success_exec.txt
+        else
+                echo "$VAR_IP - Failure" > failure_exec.txt
+        fi
+        sleep 120
+done
+
