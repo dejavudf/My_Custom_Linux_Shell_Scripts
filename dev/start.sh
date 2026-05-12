cf2ad384f86179b3d75c4b8b7957d4b736952bf3
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-05-12 10:10:36 -0300

    Create start.sh

---
+++ dev/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#variables
+VAR_CMD="show config"
+VAR_USER="teste"
+VAR_DIR="/usr/local/Extreme_Networks/NetSight/appdata/InventoryMgr/configs/cores"
+VAR_DT=$(date '+%Y%m%d');
+VAR_KEY="HostKeyAlgorithms=ssh-rsa,ssh-dss"
+VAR_STRICT="StrictHostKeyChecking=accept-new"
+VAR_ALGO="KexAlgorithms=+diffie-hellman-group1-sha1,diffie-hellman-group14-sha1"
+VAR_FILE="teste.txt"
+VAR_ACE="PubkeyAcceptedKeyTypes=ssh-rsa"
+VAR_RSA="../sub_rsa_2048_sha1"
+
+#script start
+rm ./*.log
+mkdir "$VAR_DIR/$VAR_DT"
+echo Starting Scripts...Wait...
+for VAR_IP in $(cat < $VAR_FILE)
+do
+        ssh -i $VAR_RSA -o $VAR_KEY -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP -o $VAR_ACE -o RemoteCommand="dis cli pag perm"
+        if ssh -i $VAR_RSA -o $VAR_KEY -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP -o $VAR_ACE -o RemoteCommand="$VAR_CMD" \
+        > "$VAR_DIR""/""$VAR_DT""/""$VAR_DT""_""$VAR_IP"_config.cfg
+        then
+                echo "$VAR_IP - Success" >> "$VAR_DT""_"success.log
+        else
+                echo "$VAR_IP - Failure" >> "$VAR_DT""_"failure.log
+        fi
+done
