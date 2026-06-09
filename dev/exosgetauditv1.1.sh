d6b001eccb50e01722f106494374b854faa20155
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-06-09 12:29:46 -0300

    Update exosgetauditv1.1.sh

--- dev/exosgetauditv1.1.sh
+++ dev/exosgetauditv1.1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#Created by: Alexsandro Farias (gitHub.com/dejavudf)
+#Version: 1.0
+#Description: backup audit logs from Extreme EXOS Switches
+#Linux version supported: Debian, Ubuntu and Mint
+# total SUMMIT: 197
+# total SWITCH ENGINE: 378
+
 if cd /home/netsight/scripts/bckaudit
 then
 
@@ -8,10 +15,11 @@ then
 	VAR_DIR="/usr/local/Extreme_Networks/NetSight/appdata/InventoryMgr/configs/audit"
 	VAR_DT=$(date '+%Y%m%d')
 	VAR_STRICT="StrictHostKeyChecking=accept-new"
-	VAR_ALGO="KexAlgorithms=+diffie-hellman-group1-sha1,diffie-hellman-group14-sha1"
+	VAR_ALGO="KexAlgorithms=+diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,curve25519-sha256"
 	VAR_ACE="PubkeyAcceptedKeyTypes=ssh-rsa,ssh-dss,rsa-sha2-256,rsa-sha2-512"
-        VAR_RSA="../../sub_rsa_2048_sha1"
-	VAR_FILE="ipsexos.txt"
+        VAR_HOST="HostKeyAlgorithms=ssh-dss,ssh-rsa"
+	VAR_RSA="../../sub_rsa_2048_sha1"
+	VAR_FILE="ips.txt"
 	VAR_TIMEOUT=
 
 	#get random
@@ -21,10 +29,10 @@ then
 	rm ./*.log
 	for VAR_IP in $(cat ./$VAR_FILE)
 	do
-	        if ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "dis cli pag perm"
+	        if ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "dis cli pag perm"
 	        then
 	                echo "$VAR_IP - Success" >> success_check.log
-	                ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show cli journal" > $VAR_IP.tmp
+	                ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show cli journal" > $VAR_IP.tmp
 	                cat $VAR_IP.tmp | grep -i "/" > ./$VAR_IP"_2".tmp
 	                while read -r VAR_AUDIT
 	                do
