4bd8b4b08975a3f9b4664c417cc3f4b1a9f4e624
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-06-09 12:29:05 -0300

    Update exoschkpoe_injetorv1.0.sh

--- dev/exoschkpoe_injetorv1.0.sh
+++ dev/exoschkpoe_injetorv1.0.sh
@@ -1,54 +1,83 @@
 #!/bin/bash
-#Created by: Alexsandro Farias (gitHub.com/dejavudf)
-#Version: 1.0
-#Description: backup lldp neighbours
-#Linux version supported: Debian, Ubuntu and Mint
-# total SUMMIT: 197
-# total SWITCH ENGINE: 378
 
-if cd /home/netsight/scripts/bcklldp
+if cd /home/netsight/scripts/chkpoe
 then
-
-	#variables
-	VAR_PASS=$1
-	VAR_USER="suportenoc"
-	VAR_DIR="/usr/local/Extreme_Networks/NetSight/appdata/InventoryMgr/configs/lldp"
-	VAR_DT=$(date '+%Y%m%d')
-	VAR_STRICT="StrictHostKeyChecking=accept-new"
-	VAR_ALGO="KexAlgorithms=+diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,curve25519-sha256"
-	VAR_ACE="PubkeyAcceptedKeyTypes=ssh-rsa,ssh-dss,rsa-sha2-256,rsa-sha2-512"
+	 #variables
+        VAR_PASS=$1
+        VAR_USER="suportenoc"
+        VAR_DIR="/usr/local/Extreme_Networks/NetSight/appdata/InventoryMgr/configs/poe"
+        VAR_DT=$(date '+%Y%m%d');
+        VAR_KEY="HostKeyAlgorithms=ssh-rsa,ssh-dss,rsa-sha2-256,rsa-sha2-512"
+        VAR_STRICT="StrictHostKeyChecking=accept-new"
+        VAR_ALGO="KexAlgorithms=+diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,curve25519-sha256"
         VAR_HOST="HostKeyAlgorithms=ssh-dss,ssh-rsa"
-	VAR_RSA="../../sub_rsa_2048_sha1"
-	VAR_FILE="ips.txt"
-	VAR_TIMEOUT=
+        VAR_FILE="ips.txt"
+        VAR_ACE="PubkeyAcceptedKeyTypes=ssh-rsa,ssh-dss,rsa-sha2-256,rsa-sha2-512"
 
-	#get random
-	VAR_RANDOM=$(gpg --batch --passphrase "$VAR_PASS" -d -q ../random.gpg)
+        #get random
+        VAR_RANDOM=$(gpg --batch --passphrase "$VAR_PASS" -d -q ../random.gpg)
 
 	rm ./*.tmp
 	rm ./*.log
+	rm ./*.lldp
+	rm ./*.poe
 	for VAR_IP in $(cat ./$VAR_FILE)
 	do
-	        if ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "dis cli pag perm"
-	        then
-	                echo "$VAR_IP - Success" >> success_check.log
-	                ../random -p $VAR_RANDOM ssh -i $VAR_RSA -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show lldp neighbors" > $VAR_IP.tmp
-			grep '[0-9]' ./$VAR_IP.tmp | awk '{print $1,$2,$3,$6}' > ./$VAR_IP"_2".tmp
-			while read -r VAR_LLDP
-                       	do
-				VAR_HASH=$(echo -n $VAR_LLDP | md5sum | awk '{print $1}')
-                                	if ! cat $VAR_DIR/$VAR_IP"_exos".lldp | grep -i "$VAR_HASH"
-	                              	then
-					if ! echo $VAR_LLDP | grep -i "Not-Advertised"
+		if ../random -p $VAR_RANDOM ssh -o $VAR_KEY -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "dis cli pag perm"
+		then
+			if ../random -p $VAR_RANDOM ssh -o $VAR_KEY -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show system | include SysName" > $VAR_IP"_sysname".tmp
+			then
+	        		if ../random -p $VAR_RANDOM ssh -o $VAR_KEY -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show lldp neighbors" > $VAR_IP"_lldp".tmp
+	                	then
+					if ../random -p $VAR_RANDOM ssh -o $VAR_KEY -o $VAR_ACE -o $VAR_HOST -o $VAR_STRICT -o $VAR_ALGO $VAR_USER@$VAR_IP "show inline-power stats ports" > $VAR_IP"_poe".tmp
 					then
-						echo "LLDP: $VAR_LLDP - Date: $VAR_DT - Hash: $VAR_HASH" >> $VAR_DIR/$VAR_IP"_exos".lldp | grep -i "$VAR_HASH"
+						VAR_SYSNAME=$(cat ./$VAR_IP"_sysname".tmp | awk '{print $2}')
+						grep '[0-9]' ./$VAR_IP"_lldp".tmp | awk '{print $1,$2,$3,$6}' > ./$VAR_IP"_2".tmp
+						cat ./$VAR_IP"_2".tmp | grep -w -e "ge1" -e "ge2" -e "eth0" -e "eth1" > ./$VAR_IP.lldp
+						grep '[0-9]' ./$VAR_IP"_poe".tmp | awk '{print $1,$2}' > ./$VAR_IP.poe
+			        	        while read -r VAR_LLDP
+	        		        	do
+		                		        VAR_PORT=$(echo "$VAR_LLDP" | awk '{print $1}')
+		                        		VAR_ID=$(echo "$VAR_LLDP" | awk '{print $2}')
+							VAR_PORT_ID=$(echo "$VAR_LLDP" | awk '{print $3}')
+							VAR_NAME=$(echo "$VAR_LLDP" | awk '{print $4}')
+		        	                	if ! cat ./$VAR_IP.poe | grep -iw "$VAR_PORT delivering"
+			        	                then
+        			        			if [ $VAR_NAME == "Not-Advertised" ] || [ $VAR_NAME == "Axis" ]
+								then
+									continue
+								else
+									echo $VAR_SYSNAME";"$VAR_IP";"$VAR_PORT";"$VAR_ID";"$VAR_PORT_ID";"$VAR_NAME >> $VAR_DIR/$VAR_DT"_"injetor_exos.csv
+								fi
+							fi
+		        		        done < ./$VAR_IP.lldp
+					else
+						if ! cat ./failure_no_poe.log | grep -i $VAR_IP
+						then
+							echo "$VAR_IP - No POE Support" >> failure_no_poe.log
+						fi
+					fi
+				else
+					if ! cat ./failure_check.log | grep -i $VAR_IP
+					then
+						echo "$VAR_IP - Failure" >> failure_check.log
 					fi
 				fi
-	                done < ./$VAR_IP"_2".tmp
-	        else
-	                echo "$VAR_IP - Failure" >> failure_check.log
-	        fi
+			else
+				if ! cat ./failure_check.log | grep -i $VAR_IP
+				then
+					echo "$VAR_IP - Failure" >> failure_check.log
+				fi
+			fi
+		else
+			if ! cat ./failure_check.log | grep -i $VAR_IP
+                        then
+				echo "$VAR_IP - Failure" >> failure_check.log
+			fi
+		fi
 	done
 fi
 rm ./*.tmp
-
+rm ./*.lldp
+rm ./*.poe
+exit 0
