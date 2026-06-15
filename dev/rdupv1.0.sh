1df5667cac853881fd48edd017396b44283ed203
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-06-15 13:41:50 -0300

    Update rdupv1.0.sh

--- dev/rdupv1.0.sh
+++ dev/rdupv1.0.sh
@@ -54,7 +54,6 @@ then
 	find "$VAR_DIR" -type f | while read -r VAR_FILE; do
 		clear
 		echo "Checking if file $VAR_FILE is duplicated. Please, wait!"
-		sleep 1
 		VAR_HASH=$(md5sum "$VAR_FILE" | awk '{print $1}')
 		VAR_SIZE=$(stat -c %s "$VAR_FILE")
 		if ! cat < ./unique.tmp | grep "$VAR_HASH$VAR_SIZE" > /dev/null 2>&1
