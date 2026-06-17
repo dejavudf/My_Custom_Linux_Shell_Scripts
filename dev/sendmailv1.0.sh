baea61d1a951c0a79fef9e2a9ab7bee76533dc9b
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2026-06-17 12:11:27 -0300

    Update sendmailv1.0.sh

--- dev/sendmailv1.0.sh
+++ dev/sendmailv1.0.sh
@@ -2,7 +2,7 @@
 
 #Variables
 VAR_DT=$(date '+%Y%m%d');
-VAR_SERVER="smpt.teste.com:25"
+VAR_SERVER="smtp.teste.net:25"
 VAR_FROM="alerta@teste.com.br"
 VAR_TO="teste@teste.com"
 VAR_SUBJ=""
@@ -18,10 +18,27 @@ FUNC_SEND() {
 }
 
 #script begin
+
 #validate status script backup core
 if [ -f "./bckcores/"$VAR_DT""_"failure.log" ]
 then
-        VAR_SUBJ="Erro(s) no(s) backup(s) COREs teste"
-        VAR_FILE="message-file=./bckcores/"$VAR_DT"_failure.log"
+	VAR_SUBJ="Erro no script de backup COREs"
+	VAR_FILE="message-file=./bckcores/"$VAR_DT"_failure.log"
+	FUNC_SEND
+fi
+
+#validate status script backup controladoras identify
+if [ -f "./bckcontroladoras/"$VAR_DT""_"failure.log" ]
+then
+        VAR_SUBJ="Erro no backup Controladoras"
+        VAR_FILE="message-file=./bckcontroladoras/"$VAR_DT"_failure.log"
+        FUNC_SEND
+fi
+
+#validate status script backup audit, detectar aps com injetor poe e historico lldp
+if [ -f "./bckaudit/"$VAR_DT""_"failure.log" ]
+then
+        VAR_SUBJ="Erro(s) no(s) script(s) de backup audit logs, detecta APs injetores POE e coleta histÃ³rico LLDP"
+        VAR_FILE="message-file=./bckaudit/"$VAR_DT"_failure.log"
         FUNC_SEND
 fi
